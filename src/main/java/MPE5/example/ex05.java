e1fa89c97203482f37945033e7085df57c7b6355
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 19:19:27 -0400

    Final commit

--- src/main/java/MPE5/example/ex05.java
+++ src/main/java/MPE5/example/ex05.java
@@ -1,4 +1,41 @@
 package MPE5.example;
 
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
 public class ex05 {
+    //main method begin
+    public static void main(String[] args) {
+        //scanner
+        Scanner in = new Scanner(System.in);
+
+        //inputs
+        System.out.print("What is the first number? ");
+        String firstString = in.nextLine();
+        System.out.print("What is the second number? ");
+        String secondString = in.nextLine();
+
+        //String convert
+        int firstInt =Integer.parseInt(firstString);
+        int secondInt =Integer.parseInt(secondString);
+
+        //math
+        int add = firstInt + secondInt;
+        int sub = firstInt - secondInt;
+        int mult = firstInt * secondInt;
+        int div = firstInt / secondInt;
+
+        //Output String
+        String outputString = firstInt + " + " + secondInt + " = " + add + "\n" + firstInt + " - " + secondInt + " = " + sub + "\n" + firstInt + " * " + secondInt + " = " + mult + "\n" + firstInt + " / " + secondInt + " = " + div +"\n";
+
+        //Output
+        System.out.println(outputString);
+    }
+    //main method end
 }
+//class end
